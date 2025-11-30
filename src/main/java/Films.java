33b1fb2a9e1776eae6deca90e50d7252d7fc6b6e
eriinys <eriinys@duck.com>
2025-11-30 11:28:18 -0600

    added filmId, releaseYear, and length in the field and setup getters

--- src/main/java/Films.java
+++ src/main/java/Films.java
@@ -1,5 +1,18 @@
 public class Films {
     private String title;
+    private String description;
+    private int filmId;
+    private int releaseYear;
+    private int length;
+
+
+    public Films(String title, String description, int filmId, int releaseYear, int length){
+        this.title = title;
+        this.description = description;
+        this.filmId = filmId;
+        this. releaseYear = releaseYear;
+        this.length = length;
+    }
 
     public Films(String title){
         this.title = title;
@@ -12,4 +25,31 @@ public class Films {
         this.title = title;
     }
 
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getFilmId() {
+        return filmId;
+    }
+    public void setFilmId(int filmId) {
+        this.filmId = filmId;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public int getLength() {
+        return length;
+    }
+    public void setLength(int length) {
+        this.length = length;
+    }
 }
