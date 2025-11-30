3250dc95ea3df9be53f2ea4f9f9f1f25b26321c6
eriinys <eriinys@duck.com>
2025-11-30 11:14:23 -0600

    added prompt that asks user for first and last name of actor

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -1,20 +1,30 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class UI {
     private final Scanner scanner = new Scanner(System.in);
     private DB db;
-    private Actors actor;
 
     public UI(DB db){
         this.db = db;
-        this.actor = new Actors();
     }
 
     public void HomeScreen() {
 
         System.out.println("Enter the last name of your favorite actor:");
         String lastName = scanner.nextLine().toLowerCase();
-        actor.setLastName(lastName);
         db.getLastName(lastName);
+
+        System.out.println("Enter the first name and last name of an actor you want to see the movies of:");
+        String actorName = scanner.nextLine().toLowerCase();
+        String[] nameParts = actorName.trim().split(" ");
+
+        if(nameParts.length == 2){
+            String fn = nameParts[0];
+            String ln = nameParts[1];
+            db.getFilmByActor(fn, ln);
+        } else {
+            System.out.println("Please enter both first and last name of the actor.");
+        }
     }
 }
