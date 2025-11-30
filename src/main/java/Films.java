d71b07a46edb8f8d36a2c3a307fdb3811fb9f6af
eriinys <eriinys@duck.com>
2025-11-30 11:13:48 -0600

    setup Films class

---
+++ src/main/java/Films.java
@@ -0,0 +1,15 @@
+public class Films {
+    private String title;
+
+    public Films(String title){
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
